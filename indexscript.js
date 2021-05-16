2544902393b7e36d45148675bfec028e39dbd44c
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2021-05-16 20:43:34 +0700

    Del PNG

--- indexscript.js
+++ indexscript.js
@@ -3,4 +3,4 @@
 // create new class at cardstatus 
 
 var card01 = ["azur-lane", "blank", "al-card.html", "Azur-Lane.png", "Azur Lane", "#01", "Developing"];
-var card02 = ["Unavailable", "blank", "01-card.html", "blank.png", "Unavailable", "#02", "Updating"];
+var card02 = ["Unavailable", "blank", "01-card.html", "blank", "Unavailable", "#02", "Updating"];
