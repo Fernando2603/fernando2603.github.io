f2cae3b4325ae26ca68fcdd441690a077647166b
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2021-05-16 17:55:04 +0700

    Update indexscript.js
    add card2

--- indexscript.js
+++ indexscript.js
@@ -3,3 +3,4 @@
 // create new class at cardstatus 
 
 var card01 = ["azur-lane", "blank", "al-card.html", "Azur-Lane.png", "Azur Lane", "#01", "Developing"];
+var card02 = ["Unavailable", "blank", "01-card.html", "blank.png", "Unavailable", "#02", "Updating"];
