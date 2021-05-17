2ac2abef9c74f9f58dd8bd6393d85011a0f7e54c
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2021-05-17 13:26:02 +0700

    add comment & change card 1 link

--- indexscript.js
+++ indexscript.js
@@ -2,7 +2,7 @@
 // create parent root with cardparent
 // create new id with card id
 // create new class at cardstatus 
-
-var card01 = ["card-01", "azur-lane", "blank", "al-card", "Azur-Lane.png", "Azur Lane", "#01", "Developing"];
+//   card    |parent  |  |id       |  |type |  |link||image        |  |title    |  |numbe||status    | 
+var card01 = ["card-01", "azur-lane", "blank", "AL", "Azur-Lane.png", "Azur Lane", "#01", "Developing"];
 var card02 = ["card-02", "Unavailable", "blank", "card-01.html", "blank.png", "Unavailable", "#02", "Updating"];
 
