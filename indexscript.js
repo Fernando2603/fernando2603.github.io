10ccc4d68726b43fc76a5207131caaf6332591a4
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2021-05-17 16:32:54 +0700

    Add DD Ship

--- indexscript.js
+++ indexscript.js
@@ -5,4 +5,3 @@
 //   card    |parent  |  |id       |  |type |  |link||image        |  |title    |  |numbe||status    | 
 var card01 = ["card-01", "azur-lane", "blank", "AL", "Azur-Lane.png", "Azur Lane", "#01", "Developing"];
 var card02 = ["card-02", "Unavailable", "blank", "card-01.html", "blank.png", "Unavailable", "#02", "Updating"];
-
