2a1fbe7a5d424f474a92818ddacdbe008beabb25
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2021-05-17 13:11:46 +0700

    Update indexscript.js

--- indexscript.js
+++ indexscript.js
@@ -3,6 +3,6 @@
 // create new id with card id
 // create new class at cardstatus 
 
-var card01 = ["card-01", "azur-lane", "blank", "al-card.html", "Azur-Lane.png", "Azur Lane", "#01", "Developing"];
+var card01 = ["card-01", "azur-lane", "blank", "al-card", "Azur-Lane.png", "Azur Lane", "#01", "Developing"];
 var card02 = ["card-02", "Unavailable", "blank", "card-01.html", "blank.png", "Unavailable", "#02", "Updating"];
 
