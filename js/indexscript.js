b93cd4edf08c46433660eb1e1b93661d4847fe9d
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2021-05-18 18:50:26 +0700

    test ajax

--- js/indexscript.js
+++ js/indexscript.js
@@ -2,6 +2,6 @@
 // create parent root with cardparent
 // create new id with card id
 // create new class at cardstatus 
-//   card    |parent  |  |id       |  |type |  |link||image        |  |title    |  |numbe||status    | 
-var card01 = ["card-01", "azur-lane", "blank", "AL", "Azur-Lane.png", "Azur Lane", "#01", "Developing"];
-var card02 = ["card-02", "ajaxpractice", "blank", "AJAX", "blank.png", "AJAX Practice", "#02", "Practice"];
+//   var card = [parent, id, type, AJAX, image, title, number, status]
+var card01 = ["card-01", "azur-lane", "blank", "AL-AJAX", "Azur-Lane.png", "Azur Lane", "#01", "Developing"];
+var card02 = ["card-02", "ajaxpractice", "blank", "TEST-AJAX", "blank.png", "AJAX Practice", "#02", "Practice"];
