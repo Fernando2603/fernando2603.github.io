5a82724e1854accbc31c35ae03f5e9d7c822e9ca
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2021-05-18 10:22:25 +0700

    add comment to script

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
 // Index Cards
-var A1 = '<div id="'
-var Axs =  'card-01' //Card Parent
-var A2 = '">' +
-		 '<div class="card" id="';
-var Axs = 'Azur-Lane'; //Card ID
-var A3 = '">' +
-         '<div class="cover">' +
-         '<div class="edit ';
-var Axs = 'BB/CV/CVL'; //Card Type
-var A4 = '">' +
-         '</div>' +
-         '<div class="image" onclick="location.href=\'';
-var Axs = 'al-card.html'; //Card Link
-var A5 = '\'" style="background-image: url(\'Images/';
-var Axs = 'Azurlane.png'; //Card Image
-var A6 = '\');"></div>' +
-         '</div>' +
-         '<div class="title">' +
-		 '<a href="';
-var Axs = 'al-card.html'; //Card Link
-var A7 = '">';
-var Axs = 'Azur Lane'; //Card Title
-var A8 = '</a>' + 
-         '</div>' +
-         '<div id="number">';
-var Axs = '#01'; //Card IDNumber
-var A9 = '</div>' + 
-         '<div class="progress ';
-var Axs = 'Azur-Lane' //Card Status
-var A10 = '">';
-var Axs = 'Developing'; //Card Status
-var A11 = '</div>' +
-        '</div>' +
-        '</div>';
+// function cards
+    var A1 = '<div id="'
+    var Axs =  'card-01' //Card Parent
+    var A2 = '">' +
+    		 '<div class="card" id="';
+    var Axs = 'Azur-Lane'; //Card ID
+    var A3 = '">' +
+             '<div class="cover">' +
+             '<div class="edit ';
+    var Axs = 'BB/CV/CVL'; //Card Type
+    var A4 = '">' +
+             '</div>' +
+             '<div class="image" onclick="location.href=\'';
+    var Axs = 'al-card.html'; //Card Link
+    var A5 = '\'" style="background-image: url(\'Images/';
+    var Axs = 'Azurlane.png'; //Card Image
+    var A6 = '\');"></div>' +
+             '</div>' +
+             '<div class="title">' +
+    		 '<a href="';
+    var Axs = 'al-card.html'; //Card Link
+    var A7 = '">';
+    var Axs = 'Azur Lane'; //Card Title
+    var A8 = '</a>' + 
+             '</div>' +
+             '<div id="number">';
+    var Axs = '#01'; //Card IDNumber
+    var A9 = '</div>' + 
+             '<div class="progress ';
+    var Axs = 'Azur-Lane' //Card Status
+    var A10 = '">';
+    var Axs = 'Developing'; //Card Status
+    var A11 = '</div>' +
+            '</div>' +
+            '</div>';
 
-function cards (cardparent, cid, ctype, clink, cimage, ctitle, cidnumber, cstatus) {
-        return A1+cardparent+A2+cid+A3+ctype+A4+clink+A5+cimage+A6+clink+A7+ctitle+A8+cidnumber+A9+cstatus+A10+cstatus+A11
-    }
-// Azur Lane Cards
-// create common shipcards
-// document.getElementById("demo1").innerHTML = sc1("Rare","a.html","a.png","zara","#22");
-// $("#tester").html(scard(n52[0], n52[1], n52[2], n52[3], n52[4], n52[5]))
+    function cards (cardparent, cid, ctype, clink, cimage, ctitle, cidnumber, cstatus) {
+            return A1+cardparent+A2+cid+A3+ctype+A4+clink+A5+cimage+A6+clink+A7+ctitle+A8+cidnumber+A9+cstatus+A10+cstatus+A11
+        }
 
+// function scard
+    // Azur Lane Cards
+    // create common shipcards
+    // document.getElementById("demo1").innerHTML = sc1("Rare","a.html","a.png","zara","#22");
+    // $("#tester").html(scard(n52[0], n52[1], n52[2], n52[3], n52[4], n52[5]))
     var S1 = '<div id="';
     var Sx = 'card-01' //Card Parent > fill with ship id
     var S2 = '">' +
@@ -85,27 +87,30 @@ function cards (cardparent, cid, ctype, clink, cimage, ctitle, cidnumber, cstatu
     }
 
 // AZUR LANE TAB FUNCTION
-function openCards(evt, cardName) {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
+    function openCards(evt, cardName) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(cardName).style.display = "block";
+            evt.currentTarget.className += " active";
         }
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-        document.getElementById(cardName).style.display = "block";
-        evt.currentTarget.className += " active";
-    }
 
 // AZUR LANE SHOW ALL FUNCTION 
-function allshow () {
-    $("#BB-tab").show();
-    $("#CV-tab").show();
-    $("#CA-tab").show();
-    $("#CL-tab").show();
-    $("#DD-tab").show();
-    $("#SS-tab").show();
-    $("#AUX-tab").show();   
-}
+    function allshow () {
+        $("#BB-tab").show();
+        $("#CV-tab").show();
+        $("#CA-tab").show();
+        $("#CL-tab").show();
+        $("#DD-tab").show();
+        $("#SS-tab").show();
+        $("#AUX-tab").show();   
+    }
+
+
+// blank
