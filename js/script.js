1a990578dd05b765fe112f2f63b26a001d16dab2
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2021-05-18 14:39:18 +0700

    test

--- js/script.js
+++ js/script.js
@@ -112,5 +112,19 @@
         $("#AUX-tab").show();   
     }
 
-
 // blank
+  function loadDoc(url, cFunction) {
+    var xhttp;
+    xhttp=new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        cFunction(this);
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }
+  function myFunction(xhttp) {
+    document.getElementById("demo").innerHTML =
+    xhttp.responseText;
+  }
