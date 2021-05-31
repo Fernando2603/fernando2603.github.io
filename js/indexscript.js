0fa4521a88dfcd13c5d2343c2c3fbcf674a4bc0a
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2021-05-31 11:27:13 +0700

    Update indexscript.js

--- js/indexscript.js
+++ js/indexscript.js
@@ -2,6 +2,6 @@
 // create parent root with cardparent
 // create new id with card id
 // create new class at cardstatus 
-//   var card = [parent, id, type, AJAX, image, title, number, status]
+//   var card = [parent, id, type, link, image, title, number, status]
 var card01 = ["card-01", "azur-lane", "blank", "AL", "Azur-Lane.png", "Azur Lane", "#01", "Developing"];
-var card02 = ["card-02", "ajaxpractice", "blank", "AJAX", "blank.png", "AJAX Practice", "#02", "Practice"];
+var card02 = ["card-02", "fernando", "blank", "Fernando", "Fernando.jpg", "Fernando", "#02", "Personal"];
