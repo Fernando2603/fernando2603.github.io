789da61dcad3c6b5e129cdf44b00155ca9cfde34
Fernando2603 <81207411+Fernando2603@users.noreply.github.com>
2021-06-23 13:00:47 +0700

    Update

---
+++ js/meowfficer-talent.js
@@ -0,0 +1,5 @@
+function flame () {
+	return  '<td><img src="https://azurlane.koumakan.jp/w/images/thumb/2/2f/Special_Skill.png/50px-Special_Skill.png"></td>'+
+			'<td>Flame\'s Aggression</td>'+
+			'<td>Fleet Damage +3%</td>';
+}
