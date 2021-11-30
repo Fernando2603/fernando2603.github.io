84f2109ad4ef3064494a3e40257dfc889e9ad2c6
Fernando <81207411+Fernando2603@users.noreply.github.com>
2021-11-30 14:19:30 +0700

    [30112021]

--- js/indexscript.js
+++ js/indexscript.js
@@ -1,17 +1,17 @@
 // $("#tester").html(cards(cardparent, cardid, cardtype, cardlink, cardimage, cardtitle, cardidnumber, cardstatus))
 // create parent root with cardparent
-// create new id with card id
+// create new id with card id // on patch 30112021 [id] change to [class]
 // create new class at cardstatus 
-//   var card = [parent, id, type, link, image, title, number, status]
+//   var card = [parent, class, type, link, image, title, number, status]
 var card01 = ["card-01", "azur-lane", "blank", "AL", "Azur-Lane.png", "AL", "#01", "Database"];
-var card02 = ["card-02", "event-oil", "blank", "EventOil", "Azur-Lane.png", "EventOil", "#02", "Calculator"];
-var card03 = ["card-03", "ship-banner-generator", "blank", "ShipBannerGenerator", "Azur-Lane.png", "ShipBanner", "#03", "Generator"];
-var card04 = ["card-04", "barrage", "blank", "barrage", "Azur-Lane.png", "Barrage", "#04", "Database"];
-var card05 = ["card-05", "coins", "blank", "coins", "Azur-Lane.png", "Coins", "#05", "Personal"];
-var card06 = ["card-06", "gear", "blank", "gear", "Azur-Lane.png", "Gear", "#06", "Database"];
-var card07 = ["card-07", "meowfficer", "blank", "meowfficer", "Azur-Lane.png", "Meowfficer", "#07", "Database"];
-var card08 = ["card-08", "opsi", "blank", "opsi", "Azur-Lane.png", "Operation Siren", "#08", "Personal"];
-var card09 = ["card-09", "ship", "blank", "ship", "Azur-Lane.png", "ShipList", "#09", "Database"];
-var card10 = ["card-10", "shipmax", "blank", "shipmax", "Azur-Lane.png", "Ship Max", "#10", "Personal"];
-var card11 = ["card-11", "ships", "blank", "ships", "Azur-Lane.png", "Ship Stats", "#11", "Database"];
+var card02 = ["card-02", "azur-lane", "blank", "EventOil", "Azur-Lane.png", "EventOil", "#02", "Calculator"];
+var card03 = ["card-03", "azur-lane", "blank", "ShipBannerGenerator", "Azur-Lane.png", "ShipBanner", "#03", "Generator"];
+var card04 = ["card-04", "azur-lane", "blank", "barrage", "Azur-Lane.png", "Barrage", "#04", "Database"];
+var card05 = ["card-05", "azur-lane", "blank", "coins", "Azur-Lane.png", "Coins", "#05", "Personal"];
+var card06 = ["card-06", "azur-lane", "blank", "gear", "Azur-Lane.png", "Gear", "#06", "Database"];
+var card07 = ["card-07", "azur-lane", "blank", "meowfficer", "Azur-Lane.png", "Meowfficer", "#07", "Database"];
+var card08 = ["card-08", "azur-lane", "blank", "opsi", "Azur-Lane.png", "Operation Siren", "#08", "Personal"];
+var card09 = ["card-09", "azur-lane", "blank", "ship", "Azur-Lane.png", "ShipList", "#09", "Database"];
+var card10 = ["card-10", "azur-lane", "blank", "shipmax", "Azur-Lane.png", "Ship Max", "#10", "Personal"];
+var card11 = ["card-11", "azur-lane", "blank", "ships", "Azur-Lane.png", "Ship Stats", "#11", "Database"];
 var card12 = ["card-12", "fernando", "blank", "Fernando", "Fernando.jpg", "Fernando", "#12", "Personal"];
