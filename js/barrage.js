4272f23881d1310bc41c3ff321c01ea8eef3ba8f
Fernando <81207411+Fernando2603@users.noreply.github.com>
2021-12-15 14:43:19 +0700

    [15122021]

--- js/barrage.js
+++ js/barrage.js
@@ -7,7 +7,7 @@ fetch("https://raw.githubusercontent.com/AzurAPI/azurapi-js-setup/master/barrage
       barrageCall();
       $(function () {$('[data-toggle="tooltip"]').tooltip()})
       $(".ShipOutput").append(ShipOutput)
-      $("#ShipBarrage").dataTable({searching: false, paging: false, info: false});
+      $("#ShipTable").dataTable({searching: false, paging: false, info: false});
    },
    (error) => {
       console.log('error: ' + error);
